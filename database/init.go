84859a769cdacb8cdfc9951e31c0cb4b6d282ad9
정삼복 <68502584+3boku@users.noreply.github.com>
2025-03-01 14:27:21 +0900

    feat: DB 마이그레이션 설정

--- database/init.go
+++ database/init.go
@@ -12,7 +12,7 @@ func InitDatabase() (db *gorm.DB, err error) {
 		fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Seoul",
 			os.Getenv("POSTGRES_HOST"),
 			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_PASSWORD"),
+			os.Getenv("POSTGRES_PASS"),
 			os.Getenv("POSTGRES_DB"),
 			os.Getenv("POSTGRES_PORT"))), &gorm.Config{})
 	return db, err
--- types/product.go
+++ types/product.go
@@ -1,10 +1,7 @@
 package types
 
-import "github.com/google/uuid"
-
 type Product struct {
 	BasicModel
-	ID    uuid.UUID `gorm:"id"`
-	Name  string    `gorm:"name"`
-	Price float64   `gorm:"price"`
+	Name  string  `gorm:"name"`
+	Price float64 `gorm:"price"`
 }
